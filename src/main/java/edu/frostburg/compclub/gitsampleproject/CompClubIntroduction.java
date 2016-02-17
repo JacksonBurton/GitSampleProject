1b621bfd033d0baf2193f782e80e58b874bf1e5b
jdburton0 <jdburton0@CC270-86290.frostburg.edu>
2016-02-17 16:50:33 -0500

    Added my name cause Jackson is great

--- src/main/java/edu/frostburg/compclub/gitsampleproject/CompClubIntroduction.java
+++ src/main/java/edu/frostburg/compclub/gitsampleproject/CompClubIntroduction.java
@@ -77,4 +77,8 @@ public class CompClubIntroduction {
         System.out.println("---- End Instructions ----");
         System.out.println();
     }
+    @Hello
+    public void JacksonBurton(){
+        System.out.println("My name is Jackson");
+    }
 }
